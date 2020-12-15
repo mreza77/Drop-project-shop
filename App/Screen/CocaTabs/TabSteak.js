a76a353dad7a7b3c7abbac735b5e6d235039fe7d
mohammad reza <mojaver77@gmail.com>
2020-12-15 22:43:53 +0330

    screen of Coco Tabview

---
+++ App/Screen/CocaTabs/TabSteak.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class TabSteak extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> TabSteak </Text>
+      </View>
+    );
+  }
+}
+
+export default TabSteak;
