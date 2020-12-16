ed4c96fc2a1d6b48f538f4c1bb4f2cad339dc5a0
mohammad reza <mojaver77@gmail.com>
2020-12-16 22:23:46 +0330

    change name app,js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { createStackNavigator } from 'react-navigation-stack';
 
 
 
-import Product from './App/Screen/Product';
+import Product from './App/Screen/Main';
 
 
 const HomeStack = createStackNavigator(
