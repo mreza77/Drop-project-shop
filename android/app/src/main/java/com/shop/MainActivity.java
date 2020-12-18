a3eb3a8545637b6da616fd06f5a7e60f48afd4cf
mohammad reza <mojaver77@gmail.com>
2020-12-18 15:58:50 +0330

    touche behtar

--- android/app/src/main/java/com/shop/MainActivity.java
+++ android/app/src/main/java/com/shop/MainActivity.java
@@ -1,6 +1,10 @@
 package com.shop;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate; ///+ baraie touch doroste app
+import com.facebook.react.ReactRootView; ///+
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView; ///+
+
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +16,14 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "shop";
   }
+
+   @Override  ///+
+  protected ReactActivityDelegate createReactActivityDelegate() {  ///+
+    return new ReactActivityDelegate(this, getMainComponentName()) {  ///+
+      @Override  ///+
+      protected ReactRootView createRootView() {  ///+
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);  ///+
+     }  ///+
+    };  ///+
+ }  ///+
 }
